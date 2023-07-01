8282eb4c7109708b42a579ca95e17bdd8fe00e2b
MaoYoshii <mao@g.ecc.u-tokyo.ac.jp>
2023-07-02 03:02:27 +0900

    20230702

--- src/ModInt.jl
+++ src/ModInt.jl
@@ -1,10 +1,10 @@
 module ModInt
     module modint
         import Base: \, /, *, ^, +, -, show
-        import Base: Integer,Int8,Int16,Int32,Int64,Int128
+        #import Base: Integer,Int8,Int16,Int32,Int64,Int128
         include("mint.jl")
         include("operators.jl")
-        include("types.jl")
+        #include("types.jl")
         export MInt, modulus, value 
     end
     module mod1000000007
